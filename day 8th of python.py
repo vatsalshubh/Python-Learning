5c6c5268eaccc9ead79035925a6771511cf09ea6
Vatsal Arya <vatsalarya709@gmail.com>
2023-07-29 02:17:29 +0530

    commit message

---
+++ day 8th of python.py
@@ -0,0 +1,6 @@
+# the conversio of one data type in to the another data type is known as type casting in python or type conversion in python
+# ex- int(),flot(),str(),ord() etc. 
+# a = 1
+a = 1
+# b = 2
+b = 2
